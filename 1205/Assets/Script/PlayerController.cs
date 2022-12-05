6f2edd14443bc59fa0a483dfeb9e0337e2574d08
LucienQian <jean20011116@gmail.com>
2022-12-05 12:34:42 +0800

    1205

---
+++ 1205/Assets/Script/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRB;
+    public float jumpForce = 10;
+    public float gravityMod = 1;
+
+    public bool isOnGround = true; //是否貼合地板
+    
+    void Start()
+    {
+        //獲得剛體的控制權
+        playerRB = GetComponent<Rigidbody>();
+        //playerRB.AddForce(Vector3.up * 1000); //針對此剛體施加力道.方向*力量
+        Physics.gravity = Physics.gravity * gravityMod;  //Physics.gravity *= gravityMod;
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false; //切換：false
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true; //切換回：true
+    }
+}
---
+++ 1205/Assets/Script/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    public Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    public float delayTime = 1.5f;
+    public float repeatTime = 2;
+
+    void Start()
+    {
+        InvokeRepeating("ObstacleSpawn", delayTime, repeatTime);
+    }
+
+
+    void Update()
+    {
+    }
+
+    void ObstacleSpawn()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
